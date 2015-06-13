235b214f55f4b82c4e79eeb09eda0c8c63f1c589
derAndroidPro <der.android.david@gmail.com>
2015-06-13 14:01:31 +0200

    Initial commit

---
+++ app/src/main/java/de/derandroidpro/widgethomescreentutorial/WidgetProvider1.java
@@ -0,0 +1,21 @@
+package de.derandroidpro.widgethomescreentutorial;
+
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+
+public class WidgetProvider1 extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+    }
+}
