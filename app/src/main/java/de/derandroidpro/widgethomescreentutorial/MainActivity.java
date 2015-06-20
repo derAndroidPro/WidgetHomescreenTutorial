eb7f80bd66b49f1a65339ba187ba9932b5d4c9af
derAndroidPro <der.android.david@gmail.com>
2015-06-20 14:39:46 +0200

    3. COMMIT: Der Code aus dem 2. Teil des Programmiertutorials ist nun hier verfügbar.

--- app/src/main/java/de/derandroidpro/widgethomescreentutorial/MainActivity.java
+++ app/src/main/java/de/derandroidpro/widgethomescreentutorial/MainActivity.java
@@ -1,38 +1,39 @@
 package de.derandroidpro.widgethomescreentutorial;
 
+import android.appwidget.AppWidgetManager;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity{
+
+    Button btn;
+    EditText et;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        et = (EditText) findViewById(R.id.editText);
+        btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentSendTextToWidget = new Intent(MainActivity.this, WidgetProvider1.class);
+                intentSendTextToWidget.setAction(AppWidgetManager.EXTRA_CUSTOM_EXTRAS);
+                intentSendTextToWidget.putExtra("ETTEXTEXTRAKEY", et.getText().toString());
+                sendBroadcast(intentSendTextToWidget);
+            }
+        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
--- app/src/main/java/de/derandroidpro/widgethomescreentutorial/WidgetProvider1.java
+++ app/src/main/java/de/derandroidpro/widgethomescreentutorial/WidgetProvider1.java
@@ -1,21 +1,75 @@
 package de.derandroidpro.widgethomescreentutorial;
 
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.widget.RemoteViews;
+
+import java.util.Random;
 
 public class WidgetProvider1 extends AppWidgetProvider {
 
+    String widgettextviewtext;
+    final String BACKGROUNDCOLORACTION = "sdfjkldjldkf";
+
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
+
+        Intent appopenIntent = new Intent(context, MainActivity.class);
+
+        PendingIntent appopenPendingIntent = PendingIntent.getActivity(context,0,appopenIntent,0);
+
+        remoteViews.setOnClickPendingIntent(R.id.button2, appopenPendingIntent);
+
+        remoteViews.setOnClickPendingIntent(R.id.button3,onClickPendingIntent(context,BACKGROUNDCOLORACTION) );
+
+        appWidgetManager.updateAppWidget(appWidgetIds, remoteViews);
+
+
         super.onUpdate(context, appWidgetManager, appWidgetIds);
     }
 
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
+
+        if(intent.getAction().toString() == BACKGROUNDCOLORACTION){
+            int color = Color.argb(255, new Random().nextInt(255),  new Random().nextInt(255), new Random().nextInt(255));
+            remoteViews.setInt(R.id.widget_layout, "setBackgroundColor", color);
+            updateWidgetNow(context, remoteViews);
+        }
+
+
+        if(intent.hasExtra("ETTEXTEXTRAKEY")){
+            widgettextviewtext = intent.getStringExtra("ETTEXTEXTRAKEY");
+            remoteViews.setTextViewText(R.id.textView, widgettextviewtext);
+            updateWidgetNow(context, remoteViews);
+        }
+
+
         super.onReceive(context, intent);
     }
+
+  public void updateWidgetNow (Context context, RemoteViews remoteViews){
+      ComponentName widgetComponent = new ComponentName(context, WidgetProvider1.class);
+      AppWidgetManager.getInstance(context).updateAppWidget(widgetComponent, remoteViews);
+  }
+
+
+    public PendingIntent onClickPendingIntent (Context context, String stringAction){
+
+        Intent onClickIntent = new Intent(context, WidgetProvider1.class);
+        onClickIntent.setAction(stringAction);
+
+        return PendingIntent.getBroadcast(context,0,onClickIntent,0);
+    }
+
+
 }
